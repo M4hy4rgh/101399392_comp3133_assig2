5caec40c1bbc9262b4ae1b0ebe6b4aa9474a34ac
M4hy4rgh <mahyarghasemi81@gmail.com>
2024-04-11 03:01:54 -0400

    added the logout botton and fixed deployment error.

--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -21,121 +21,127 @@ import { GET_ALL_EMPLOYEES, DELETE_EMPLOYEE } from '../graphql/graphql.queries';
     styleUrl: './employee-list.component.css',
 })
 export class EmployeeListComponent implements OnInit {
-
     #data: Employee_model[] = [];
     error: any;
     loading: boolean = false;
-    anyCheckboxChecked: boolean = false; 
+    anyCheckboxChecked: boolean = false;
 
-    ngOnInit(): void { 
+    ngOnInit(): void {
         this.getAllEmployees();
-        
     }
 
     constructor(
         private router: Router,
         private apollo: Apollo,
         @Inject(PLATFORM_ID) private platformId: Object,
-        private authService: AuthServiceService,
+        private authService: AuthServiceService
     ) { }
 
     async getAllEmployees() {
         // console.log("getAllEmployees");
         this.loading = true;
-        this.apollo.watchQuery({
-            query: GET_ALL_EMPLOYEES
-        }).valueChanges.subscribe({
-            next: (res: any) => {
-                // console.log("getAllEmployees -> ", res);
-                if (res.errors) {
-                    this.error = res.errors;
-                    console.log("getAllEmployees -> ", this.error);
-                    this.loading = false;
-                }
+        this.apollo
+            .watchQuery({
+                query: GET_ALL_EMPLOYEES,
+            })
+            .valueChanges.subscribe({
+                next: (res: any) => {
+                    // console.log("getAllEmployees -> ", res);
+                    if (res.errors) {
+                        this.error = res.errors;
+                        console.log('getAllEmployees -> ', this.error);
+                        this.loading = false;
+                    }
 
-                if (res.data) {
-                    this.#data = res.data.getAllEmployees;
-                    console.log("getAllEmployees -----> ", this.#data);
-                    // this.loading = false;
+                    if (res.data) {
+                        this.#data = res.data.getAllEmployees;
+                        console.log('getAllEmployees -----> ', this.#data);
+                        // this.loading = false;
+                        setTimeout(() => {
+                            this.loading = false;
+                        }, 1500);
+                    }
+                },
+                error: (error) => {
+                    console.log('getAllEmployees -> ', error);
+                    this.error = error;
+                    this.loading = false;
+                },
+                complete: () => {
                     setTimeout(() => {
                         this.loading = false;
                     }, 1500);
-                }
-            },
-            error: (error) => {
-                console.log("getAllEmployees -> ", error);
-                this.error = error;
-                this.loading = false;
-            },
-            complete: () => {
-                setTimeout(() => {
-                    this.loading = false;
-                }, 1500);
-            }
-        });
+                },
+            });
     }
 
     /**
-     *   console.log("deleteEmployee -> ", res);
-            if (res.errors) {
-                this.error = res.errors;
-                console.log("deleteEmployee -> ", this.error);
-                this.loading = false;
-            }
-
-            if (res.data) {
-                this.#data = res.data.deleteEmployee;
-                console.log("deleteEmployee -> ", this.#data);
-                this.loading = false;
-            }
-     */
+       *   console.log("deleteEmployee -> ", res);
+              if (res.errors) {
+                  this.error = res.errors;
+                  console.log("deleteEmployee -> ", this.error);
+                  this.loading = false;
+              }
+  
+              if (res.data) {
+                  this.#data = res.data.deleteEmployee;
+                  console.log("deleteEmployee -> ", this.#data);
+                  this.loading = false;
+              }
+       */
 
     async deleteEmployee(_id: string) {
-        console.log("deleteEmployee -> ", _id);
-        this.apollo.mutate({
-            mutation: DELETE_EMPLOYEE,
-            variables: {
-                _id: _id
-            },
-            refetchQueries: [{
-                query: GET_ALL_EMPLOYEES
-            }]
-        }).subscribe({
-            next: (res: any) => {
-                console.log("deleteEmployee -> ", res);
-                if (res.errors) {
-                    this.error = res.errors;
-                    console.log("deleteEmployee -> ", this.error);
-                    setTimeout(() => {
-                        this.loading = false;
-                    }, 1200);
-                }
+        console.log('deleteEmployee -> ', _id);
+        this.apollo
+            .mutate({
+                mutation: DELETE_EMPLOYEE,
+                variables: {
+                    _id: _id,
+                },
+                refetchQueries: [
+                    {
+                        query: GET_ALL_EMPLOYEES,
+                    },
+                ],
+            })
+            .subscribe({
+                next: (res: any) => {
+                    console.log('deleteEmployee -> ', res);
+                    if (res.errors) {
+                        this.error = res.errors;
+                        console.log('deleteEmployee -> ', this.error);
+                        setTimeout(() => {
+                            this.loading = false;
+                        }, 1200);
+                    }
 
-                if (res.data) {
-                    this.#data = res.data.deleteEmployee;
-                    console.log("deleteEmployee -> ", this.#data);
+                    if (res.data) {
+                        this.#data = res.data.deleteEmployee;
+                        console.log('deleteEmployee -> ', this.#data);
+                        setTimeout(() => {
+                            this.loading = false;
+                        }, 1200);
+                    }
+                },
+                error: (error) => {
+                    console.log('deleteEmployee -> ', error);
+                    this.error = error;
                     setTimeout(() => {
                         this.loading = false;
                     }, 1200);
-                }
-            },
-            error: (error) => {
-                console.log("deleteEmployee -> ", error);
-                this.error = error;
-                setTimeout(() => {
-                    this.loading = false;
-                }, 1200);
-            },
-            complete: () => {
-                setTimeout(() => {
-                    this.loading = false;
-                }, 1200);
-            }
-        });
+                },
+                complete: () => {
+                    setTimeout(() => {
+                        this.loading = false;
+                    }, 1200);
+                },
+            });
     }
 
     deleteSelectedItems() {
-        const checkboxes = document.querySelectorAll('.checkbox:checked') as NodeListOf<HTMLInputElement>;
+        const checkboxes = document.querySelectorAll(
+            '.checkbox:checked'
+        ) as NodeListOf<HTMLInputElement>;
         checkboxes.forEach((checkbox) => {
             const id = checkbox.value;
             this.loading = true;
@@ -143,16 +149,17 @@ export class EmployeeListComponent implements OnInit {
             this.anyCheckboxChecked = false;
         });
     }
-    
 
     selectAllCheckbox(event: Event) {
-        const checkboxes = document.querySelectorAll('.checkbox') as NodeListOf<HTMLInputElement>;
+        const checkboxes = document.querySelectorAll(
+            '.checkbox'
+        ) as NodeListOf<HTMLInputElement>;
         if (checkboxes.length > 0) {
             let anyChecked = false;
             checkboxes.forEach((checkbox) => {
                 checkbox.checked = (event.target as HTMLInputElement).checked;
                 if (checkbox.checked) {
-                    anyChecked = true; 
+                    anyChecked = true;
                 }
             });
             this.anyCheckboxChecked = anyChecked;
@@ -161,11 +168,12 @@ export class EmployeeListComponent implements OnInit {
             this.anyCheckboxChecked = false;
         }
     }
-    
 
     selectCheckbox(event: Event, id: string) {
-        console.log("selectCheckbox -> ", id);
-        const checkboxes = document.querySelectorAll('.checkbox') as NodeListOf<HTMLInputElement>;
+        console.log('selectCheckbox -> ', id);
+        const checkboxes = document.querySelectorAll(
+            '.checkbox'
+        ) as NodeListOf<HTMLInputElement>;
         if (checkboxes.length > 0) {
             let allChecked = true;
             checkboxes.forEach((checkbox) => {
@@ -173,14 +181,19 @@ export class EmployeeListComponent implements OnInit {
                     allChecked = false;
                 }
             });
-            (document.querySelector('.selectAll') as HTMLInputElement).checked = allChecked;
-            this.anyCheckboxChecked = Array.from(checkboxes).some(checkbox => checkbox.checked);
+            (document.querySelector('.selectAll') as HTMLInputElement).checked =
+                allChecked;
+            this.anyCheckboxChecked = Array.from(checkboxes).some(
+                (checkbox) => checkbox.checked
+            );
         } else {
             console.error('No checkboxes found');
         }
     }
 
-
+    logout() {
+        this.authService.logout();
+    }
 
     get data() {
         return this.#data;
@@ -189,5 +202,4 @@ export class EmployeeListComponent implements OnInit {
     set data(data: Employee_model[]) {
         this.#data = data;
     }
-
 }
