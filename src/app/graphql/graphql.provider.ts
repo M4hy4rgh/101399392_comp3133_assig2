dc61756a053251204862d198b8e7b796e0eca398
M4hy4rgh <mahyarghasemi81@gmail.com>
2024-04-12 13:19:51 -0400

    updated backend to be deployed on vercel

--- src/app/graphql/graphql.provider.ts
+++ src/app/graphql/graphql.provider.ts
@@ -3,7 +3,8 @@ import { HttpLink } from 'apollo-angular/http';
 import { ApplicationConfig, inject } from '@angular/core';
 import { ApolloClientOptions, InMemoryCache } from '@apollo/client/core';
 
-const uri = 'https://one01399392-comp3133-assignment1.onrender.com/graphql';
+// const uri = 'https://one01399392-comp3133-assignment1.onrender.com/graphql';
+const uri = 'https://101399392-comp-3133-assignment1-ten.vercel.app/graphql';
 
 export function createApollo(): ApolloClientOptions<any> {
     const httpLink = inject(HttpLink);
